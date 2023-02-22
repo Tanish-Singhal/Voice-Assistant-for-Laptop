e6180626e5e8e7233d54f9d83dadaee65aaa3412
Tanish Singhal <114289953+Tanish-Singhal@users.noreply.github.com>
2023-02-22 21:12:59 +0530

    wishMe function added

--- voice_assistant.py
+++ voice_assistant.py
@@ -11,3 +11,33 @@ voices = engine.getProperty('voices')
 # print(voices[0].id)
 engine.setProperty('voice', voices[0].id)
 engine.setProperty("rate",150)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+print("\n")
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        print("Good Morning! Tanish")
+        speak("Good Morning! Tanish")
+       
+    elif hour>=12 and hour<18:
+        print("Good Afternoon! Tanish")
+        speak("Good Afternoon! Tanish")
+           
+    else:
+        print("Good Evening! Tanish")
+        speak("Good Evening! Tanish")
+        
+
+    print("Zira is at your Service sir!")
+    speak("Zira is at your Service sir!")
+
+    print("Please Tell me how may I help you.....")
+    speak("Please tell me how may I help you.....")    
+
